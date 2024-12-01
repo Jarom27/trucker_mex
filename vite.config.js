7f1f05b7da4f5470a5dad22eae25e30427e5f209
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-11-30 20:10:24 -0700

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: process.env.PORT || 3000,
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost',
+        target: 'https://truckerapi.onrender.com/',
         changeOrigin: true,
       },
     },
